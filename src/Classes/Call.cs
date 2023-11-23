8bdeba34b42b95839102795d39ec968910e97238
LionShareZoki <pujic.zoran@gmail.com>
2023-11-23 10:57:54 +0100

    Create Call class

---
+++ src/Classes/Call.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoneBook.Classes
+{
+    public class Call
+    {
+        public DateTime CallTime { get; }
+        public CallStatus Status { get; set; }
+
+
+        public Call(DateTime callTime, CallStatus status)
+        {
+            CallTime = callTime;
+            Status = status;
+        }
+    }
+
+    public enum CallStatus
+    {
+        InProgress,
+        Missed,
+        Completed
+    }
+}
