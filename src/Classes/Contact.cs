6af7f1f06202b1a828bde377e1a8c5542489edca
LionShareZoki <pujic.zoran@gmail.com>
2023-11-23 10:58:04 +0100

    Create Contact class

---
+++ src/Classes/Contact.cs
@@ -0,0 +1,35 @@
+﻿public class Contact
+{
+    public string FullName { get; }
+    public string PhoneNumber { get; }
+    public ContactPreference Preference { get; set; }
+
+    public Contact(string fullName, string phoneNumber, ContactPreference preference)
+    {
+        FullName = fullName;
+        PhoneNumber = phoneNumber;
+        Preference = preference;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Contact otherContact)
+        {
+            return PhoneNumber == otherContact.PhoneNumber;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return PhoneNumber.GetHashCode();
+    }
+    public enum ContactPreference
+    {
+        Favorit,
+        Normalan,
+        Blokiran
+    }
+}
+
