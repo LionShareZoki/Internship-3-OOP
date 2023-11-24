25b1c05a04e884dc00269c592e287f2483a3b8a2
LionShareZoki <pujic.zoran@gmail.com>
2023-11-24 16:16:54 +0100

    Create Dictionary GetPhoneBook

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+        Dictionary<Contact, List<Call>> GetPhoneBook()
+        {
+            return phoneBook;
+        }
