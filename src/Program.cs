8ffbb3b921ed1ffd25997707a0e448c46aaa1f80
LionShareZoki <pujic.zoran@gmail.com>
2023-11-24 16:17:28 +0100

    Implement IsCallInProgress method

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,11 @@
+        static bool IsCallInProgress(Program program, Contact contact)
+        {
+            if (program.GetPhoneBook().TryGetValue(contact, out List<Call> callsForContact))
+            {
+                return callsForContact.Any(call => call.Status == CallStatus.InProgress);
+            }
+            return false;
+        }
         Dictionary<Contact, List<Call>> GetPhoneBook()
         {
             return phoneBook;
