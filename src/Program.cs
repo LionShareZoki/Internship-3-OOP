a96e858230b5491742ce0a616cbcd2c0522ef1ea
LionShareZoki <pujic.zoran@gmail.com>
2023-11-24 16:17:54 +0100

    Implement PrintAllCalls method

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,33 @@
+        public void PrintAllCalls()
+        {
+            Console.Clear();
+            Console.WriteLine("Svi pozivi u imeniku:");
+
+            foreach (var entry in phoneBook)
+            {
+                var contact = entry.Key;
+                var calls = entry.Value;
+
+                Console.WriteLine($"Pozivi za kontakt {contact.FullName}:");
+
+                if (calls.Any())
+                {
+                    foreach (var call in calls)
+                    {
+                        Console.WriteLine($"Vrijeme poziva: {call.CallTime}, Status: {call.Status}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nema poziva za prikaz za odabranog kontakta.");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
         static bool IsCallInProgress(Program program, Contact contact)
         {
             if (program.GetPhoneBook().TryGetValue(contact, out List<Call> callsForContact))
